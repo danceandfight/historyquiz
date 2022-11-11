5585b76170602f811335a16767071fab581f6395
danceandfight <danceandfight@gmail.com>
2022-11-11 17:50:01 +0400

    Get questions from all files in quiz directory

--- quiz_questions_loader.py
+++ quiz_questions_loader.py
@@ -5,16 +5,17 @@ from dotenv import load_dotenv
 def get_quiz_questions():
     quiz_directory = os.getenv('QUIZ_DIRECTORY')
     filenames = os.listdir(quiz_directory)
-    with open(f'{quiz_directory}/{filenames[0]}', encoding='KOI8-R') as f:
-        quiz_questions = f.read()
-    quiz_question_chunks = quiz_questions.split('\n\n')
     question_answer_pairs = {}
-    for chunk in quiz_question_chunks:
-        if chunk.startswith('Вопрос'):
-            key = chunk
-        if chunk.startswith('Ответ'):
-            value = chunk
-            question_answer_pairs[key] = value
+    for filename in filenames:
+        with open(f'{quiz_directory}/{filename}', encoding='KOI8-R') as f:
+            quiz_questions = f.read()
+        quiz_question_chunks = quiz_questions.split('\n\n')
+        for chunk in quiz_question_chunks:
+            if chunk.startswith('Вопрос'):
+                key = chunk
+            if chunk.startswith('Ответ'):
+                value = chunk
+                question_answer_pairs[key] = value
     return question_answer_pairs
 
 if __name__ == '__main__':
